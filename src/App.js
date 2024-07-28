9702fa7307dda29b8da828b345e891b1d804aa9e
Amogh Saxena <amoghsaxena24@gmail.com>
2024-07-28 18:07:04 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BasicInfo from './Components/BasicInfo';
+import AdditionalQuestions from './Components/AdditionalQuestions'; 
+import EnteredDetails from './Components/EnteredDetails'; 
+import ThankYouPage from './Components/ThankYouPage'; 
+import { About } from './Components/About'; 
 
 function App() {
   const initBasicData = JSON.parse(localStorage.getItem('data')) || {};
@@ -32,13 +37,50 @@ function App() {
 
   const addQuestionData = (profession, interest, reference) => {
     const myQuestionData = {
-        profession: profession,
-        interest: interest,
-        reference: reference
+      profession: profession,
+      interest: interest,
+      reference: reference
     };
 
     setQuestionData(myQuestionData);
     localStorage.setItem("questiondata", JSON.stringify(myQuestionData));
-}
+  }
+
+  return (
+    <Router>
+      {/* Define the routes */}
+      <Routes>
+        {/* Render the BasicInfo component with the addBasicData function */}
+        <Route path='/' element={<BasicInfo addBasicData={addBasicData} />} />
+
+        {/* Render the AdditionalQuestions component with the addQuestionData function */}
+        <Route
+          path='/questions'
+          element={<AdditionalQuestions addQuestionData={addQuestionData} />}
+        />
+
+        {/* Render the EnteredDetails component with basicData and questionData */}
+        <Route
+          path='/details'
+          element={<EnteredDetails data={basicData} questiondData={questionData} />}
+        />
+
+        {/* Render the ThankYouPage component */}
+        <Route
+          path='/thanks'
+          element={<ThankYouPage />}
+        />
+
+        {/* Render the About component */}
+        <Route
+          path='/about'
+          element={<About />}
+        />
+      </Routes>
+    </Router>
+
+  )
 
 }
+
+export default App;
